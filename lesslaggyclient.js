3b893bd9cd3dd2dfade5cdd9276dbb0bef371443
Gradyruan7 <gradyruan@gmail.com>
2025-11-05 15:44:59 -0500

    Add fetch request to load external JavaScript

---
+++ lesslaggyclient.js
@@ -0,0 +1,2 @@
+//made my marcus not by me
+fetch('https://raw.githubusercontent.com/Gradyruan7/Updated-Bloxd-cheat/refs/heads/main/client.js').then(r=>r.text()).then(code=>console.log(code)).catch(e=>console.error(e));
